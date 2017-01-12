c8a30289178ac7c902cbb645070c3cf0132928b0
Joe Stowers <greenchiletexan@gmail.com>
2017-01-11 23:00:29 -0700

    Add test file index.js and export to indexSpec.js

--- indexSpec.js
+++ indexSpec.js
@@ -5,10 +5,22 @@ let chai = require('chai');
 let expect = chai.expect;
 let should = chai.should;
 
+var word = require('./src/index');
+
 describe('Sanitize', function() {
 	it('returns lowercase of a string', function() {
-		expect('HELLO WORLD').to.equal('HELLO world');
+
+		var inputWord = 'HELLO WORLD';
+		var outputWord = word.sanitize(inputWord);
+		expect(outputWord).to.equal('hello world');
 	});
 
-	it('removes any hyphen')
+	it('removes any hyphen');
+
+	it('tests different assertions', function() {
+		let word = 'hello world';
+		expect(word).to.not.equal('HELLO WORLD');
+		expect(word).to.contain('hello');
+		expect(word).to.be.a('string');
+	})
 })
